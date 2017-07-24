9728d6ab8a2f599e6ac7632612ef6bb6a7c3156c
Noturno <Noturno@192.168.7.19>
2017-07-24 19:27:13 -0300

    commit

--- Banda/src/br/com/banda/model/TesteEstilo.java
+++ Banda/src/br/com/banda/model/TesteEstilo.java
@@ -2,4 +2,15 @@ package br.com.banda.model;
 
 public class TesteEstilo {
 
+	public static void main(String[] args) {
+		
+		Estilo estilo1 = new Estilo();
+		Estilo estilo2 = new Estilo();
+		Estilo estilo3 = new Estilo();
+		
+		estilo1.setEst_Nome("Rock in Roll");
+		System.out.println("Estilo Musical: "+ estilo1.getEst_Nome());
+		
+		
+	}
 }
